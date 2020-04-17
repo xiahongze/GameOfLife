c34d215673c641854ea8da54b27ec3cf8b6e8c6c
Hongze Xia <hongze.x@gmail.com>
2020-04-17 20:53:02 +1000

    Initial Commit

---
+++ GameOfLife/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GameOfLife
+//
+//  Created by Hongze Xia on 17/4/20.
+//  Copyright © 2020 Hongze Xia. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
