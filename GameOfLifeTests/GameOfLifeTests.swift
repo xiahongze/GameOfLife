4c3cd9614984c8d7aecf49fd762c331338c39632
Hongze Xia <hongze.x@gmail.com>
2020-05-24 13:38:27 +1000

    added two tests for world

--- GameOfLifeTests/GameOfLifeTests.swift
+++ GameOfLifeTests/GameOfLifeTests.swift
@@ -19,9 +19,35 @@ class GameOfLifeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testWorldFlip() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let world = World(20, 20)
+        world.randomize(0.5)
+        let s1010 = world.getState(10, 10)
+        world.flipState(10, 10)
+        XCTAssertTrue(s1010 == !world.getState(10, 10))
+        let s1515 = world.getState(15, 15)
+        let s1617 = world.getState(16, 17)
+        world.flipState(at: [(15, 15), (16, 17)])
+        XCTAssertFalse(s1515 == world.getState(15, 15))
+        XCTAssertNotEqual(s1617, world.getState(16, 17))
+    }
+
+    func testWorldStep() throws {
+        let world = World(20, 20)
+        world.randomize(0.5)
+        for _ in 0..<5 {
+            var diff = world.step()
+            var diffNew = world.stepNew()
+            diff.sort(by: { a, b in (a.0 < b.0 || (a.0 == b.0 && a.1 < b.1)) })
+            diffNew.sort(by: { a, b in (a.0 < b.0 || (a.0 == b.0 && a.1 < b.1)) })
+            XCTAssertEqual(diff.count, diffNew.count)
+            for i in 0..<diff.count {
+                XCTAssertEqual(diff[i].0, diffNew[i].0)
+                XCTAssertEqual(diff[i].1, diffNew[i].1)
+            }
+        }
     }
 
     func testPerformanceExample() throws {
